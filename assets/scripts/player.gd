b00106be5f1787ded3a789292b02e4ebdaa40a5b
Kevin Greenwood <kevinrecalde324@outlook.com>
2025-01-02 10:18:12 -0500

    Update commit

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,12 +1,8 @@
 extends RigidBody2D
 
-var thrust = 700
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if Input.is_action_pressed("up"):
-		apply_central_force(thrust * global_transform.x)
-		linear_damp = 0.5
-	else:
-		linear_damp = 0
-	var mouse_pos = get_global_mouse_position()
+		apply_central_force(700 * transform.x)
+	constant_force = 4 * transform.y
 	state.transform = state.transform.looking_at(get_global_mouse_position())
